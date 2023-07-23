f1b3ca89f844886ca2e4e77233908afdd0ecfe77
Thales Spanhol <thales_spanhol@hotmail.com>
2023-07-23 13:58:14 -0300

    Feat: game tic-tac-toe

---
+++ src/components/Game.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+
+const initialBoard = Array(9).fill(null);
+
+const Game = () => {
+  const [board, setBoard] = useState(initialBoard);
+  const [isXNext, setIsXNext] = useState(true);
+
+  const handleClick = (index) => {
+    if (calculateWinner(board) || board[index]) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[index] = isXNext ? "X" : "O";
+
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <button className="square" onClick={() => handleClick(index)}>
+        {board[index]}
+      </button>
+    );
+  };
+
+  const winner = calculateWinner(board);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${isXNext ? "X" : "O"}`;
+
+  return (
+    <div className="game">
+      <div className="board">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      <div className="status">{status}</div>
+      <button className="reset-button" onClick={() => setBoard(initialBoard)}>
+        Reset
+      </button>
+    </div>
+  );
+};
+
+const calculateWinner = (board) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (const line of lines) {
+    const [a, b, c] = line;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+
+  return null;
+};
+
+export default Game;
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import Game from './components/Game.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <Game />
+  </React.StrictMode>,
+)
